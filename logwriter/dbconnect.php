42306bf6616f87727cbedb14d902c5ec207f6037
NadirHajiyev <nadirhajiyev@gmail.com>
2014-01-30 15:21:15 +0400

    logwiter0.1

--- logwriter/dbconnect.php
+++ logwriter/dbconnect.php
@@ -1,16 +1,20 @@
 <?php 
-$localhost='localhost';
-$username='root';
-$password='root';
-$dbname='log_database';*/
-$con=mysqli_connect("localhost","root","root","log_database");
+
+$host="localhost";
+$username="root";
+$password="root";
+$dbname="log_database";
+$con=mysqli_connect($host, $username,$password,$dbname);
+
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
   }
 $sql="INSERT INTO log_table (log_name, log_description)
 VALUES
 ('$_GET[logname]','$_GET[logdescription]')";
+
 if (!mysqli_query($con,$sql))
   {
   die('Error: ' . mysqli_error($con));
